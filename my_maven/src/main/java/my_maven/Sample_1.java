f8f578c8f3ed4d610236f9f6620c0cbc3fb3cf3d
Ebin Johnson <Ebin Johnson@LAP1267>
2024-08-09 10:48:37 +0530

    My First Gitpush

---
+++ my_maven/src/main/java/my_maven/Sample_1.java
@@ -0,0 +1,12 @@
+package my_maven;
+
+public class Sample_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String a="Hello.. welcome to Obsqura";
+		System.out.println(a);
+	}
+
+}
